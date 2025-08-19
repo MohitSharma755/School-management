ba07d44a416dac0b94a24803e612f86a60db8c17
mohit sharma <mohitsharma755k@gmail.com>
2025-08-19 21:16:01 +0530

    Logout

--- resources/views/admin/admindashboard.blade.php
+++ resources/views/admin/admindashboard.blade.php
@@ -12,11 +12,11 @@
     <script src="{{ asset('Js/jquery.js') }}"></script>
     <!-- 3. Bootstrap JS -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
 
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
 
 </head>
--- resources/views/spanel.blade.php
+++ resources/views/spanel.blade.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="{{ asset('Bootstrap Css/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('Custom Css/custom.css') }}">
     <script src="{{ asset('Js/custom.js') }}"></script>
+    <script src="{{ asset('Bootstrap Css/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('Js/jquery.js') }}"></script>
+    <!-- 3. Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
 
@@ -81,17 +85,27 @@
                <a href="http://" target="_blank" rel="noopener noreferrer"> <li class="list-group-item">Library </li></a>
 
 
-                <a href="http://" target="_blank" rel="noopener noreferrer"> <li class="list-group-item"> <svg height="30" width="30" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                        <g fill="none" strokeLinejoin="round" strokeWidth="4">
-                            <path
-                                d="M36.686 15.171C37.9364 16.9643 38.8163 19.0352 39.2147 21.2727H44V26.7273H39.2147C38.8163 28.9648 37.9364 31.0357 36.686 32.829L40.0706 36.2137L36.2137 40.0706L32.829 36.686C31.0357 37.9364 28.9648 38.8163 26.7273 39.2147V44H21.2727V39.2147C19.0352 38.8163 16.9643 37.9364 15.171 36.686L11.7863 40.0706L7.92939 36.2137L11.314 32.829C10.0636 31.0357 9.18372 28.9648 8.78533 26.7273H4V21.2727H8.78533C9.18372 19.0352 10.0636 16.9643 11.314 15.171L7.92939 11.7863L11.7863 7.92939L15.171 11.314C16.9643 10.0636 19.0352 9.18372 21.2727 8.78533V4H26.7273V8.78533C28.9648 9.18372 31.0357 10.0636 32.829 11.314L36.2137 7.92939L40.0706 11.7863L36.686 15.171Z"
-                                fill="#2F88FF" stroke="#000" />
-                            <path
-                                d="M24 29C26.7614 29 29 26.7614 29 24C29 21.2386 26.7614 19 24 19C21.2386 19 19 21.2386 19 24C19 26.7614 21.2386 29 24 29Z"
-                                fill="#43CCF8" stroke="#fff" />
-                        </g>
-                    </svg> Settings
-                </li></a> 
+               <button type="" class="button" data-toggle="collapse" data-target="#st">
+                 
+                <li class="list-group-item">
+                    <svg height="30" width="30" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="none" strokeLinejoin="round" strokeWidth="4">
+                        <path
+                            d="M36.686 15.171C37.9364 16.9643 38.8163 19.0352 39.2147 21.2727H44V26.7273H39.2147C38.8163 28.9648 37.9364 31.0357 36.686 32.829L40.0706 36.2137L36.2137 40.0706L32.829 36.686C31.0357 37.9364 28.9648 38.8163 26.7273 39.2147V44H21.2727V39.2147C19.0352 38.8163 16.9643 37.9364 15.171 36.686L11.7863 40.0706L7.92939 36.2137L11.314 32.829C10.0636 31.0357 9.18372 28.9648 8.78533 26.7273H4V21.2727H8.78533C9.18372 19.0352 10.0636 16.9643 11.314 15.171L7.92939 11.7863L11.7863 7.92939L15.171 11.314C16.9643 10.0636 19.0352 9.18372 21.2727 8.78533V4H26.7273V8.78533C28.9648 9.18372 31.0357 10.0636 32.829 11.314L36.2137 7.92939L40.0706 11.7863L36.686 15.171Z"
+                            fill="#2F88FF" stroke="#000" />
+                        <path
+                            d="M24 29C26.7614 29 29 26.7614 29 24C29 21.2386 26.7614 19 24 19C21.2386 19 19 21.2386 19 24C19 26.7614 21.2386 29 24 29Z"
+                            fill="#43CCF8" stroke="#fff" />
+                    </g>
+                </svg> 
+                Settings
+                </li>
+            </button>
+            <div id="st" class="collapse">
+
+                <div class="list-group-item text-danger" >Logout</div>
+
+            </div>
                   
                 <!-- A long list of placeholder items to enable scrolling -->
 
